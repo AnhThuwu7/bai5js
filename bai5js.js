4c569c12a8cc318d057409dc27f8d130dfa48383
jusst thư <anhthudoraemon7@gmail.com>
2025-04-19 17:43:59 +0700

    Add files via upload

---
+++ bai5js.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("buy-button");
+
+    button.addEventListener("click", function () {
+        // Xử lý khi nhấn nút đặt mua (chỉ cần ghi nhận hành động)
+        console.log("Người dùng đã nhấn ĐẶT MUA");
+    });
+});
